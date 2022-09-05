06d407f7bd5c2042b546ac7a9c9a1a8e15ec2124
Yana <ba.ya.na@mail.ru>
2022-09-05 14:05:18 +0500

    Adding a website translation system to Spanish

--- myshop/settings.py
+++ myshop/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'orders.apps.OrdersConfig',
     'payment.apps.PaymentConfig',
     'coupons.apps.CouponsConfig',
+    'rosetta',
 ]
 
 MIDDLEWARE = [
--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 
 app_name = 'orders'
 
